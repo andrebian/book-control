37b8fa1e7e5e5bf0192ef3c2f36656f9455ae619
Andre Cardoso <andrecardosodev@gmail.com>
2015-12-20 01:36:15 -0200

    Adicionado nova documentação de banco de dados

--- doc/database.sql
+++ doc/database.sql
@@ -6,81 +6,73 @@ CREATE SCHEMA IF NOT EXISTS `book_control` DEFAULT CHARACTER SET utf8 COLLATE ut
 USE `book_control` ;
 
 -- -----------------------------------------------------
--- Table `book_control`.`groups`
+-- Table `groups`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `book_control`.`groups` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
-  `name` VARCHAR(60) NOT NULL ,
-  `created` DATETIME NULL ,
-  `modified` DATETIME NULL ,
-  PRIMARY KEY (`id`) )
-ENGINE = MyISAM
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_unicode_ci;
-
+CREATE TABLE `groups` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- -----------------------------------------------------
--- Table `book_control`.`users`
+-- Table `users`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `book_control`.`users` (
-  `id` INT NOT NULL AUTO_INCREMENT ,
-  `group_id` INT UNSIGNED NOT NULL ,
-  `name` VARCHAR(100) NOT NULL ,
-  `surname` VARCHAR(100) NOT NULL ,
-  `email` VARCHAR(255) NOT NULL ,
-  `username` VARCHAR(255) NOT NULL ,
-  `password` VARCHAR(60) NOT NULL ,
-  `address` VARCHAR(255) NOT NULL ,
-  `addr_number` INT NOT NULL ,
-  `addr_complement` VARCHAR(50) NULL ,
-  `addr_district` VARCHAR(100) NOT NULL ,
-  `addr_city` VARCHAR(100) NOT NULL ,
-  `addr_state` VARCHAR(2) NOT NULL DEFAULT 'PR' ,
-  `addr_country` VARCHAR(45) NOT NULL ,
-  `addr_zip_code` VARCHAR(10) NOT NULL ,
-  `phone` VARCHAR(14) NOT NULL ,
-  `celphone` VARCHAR(15) NULL ,
-  `created` DATETIME NULL ,
-  `modified` DATETIME NULL ,
-  PRIMARY KEY (`id`) )
-ENGINE = MyISAM
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_unicode_ci;
-
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `group_id` int(10) NOT NULL,
+  `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `surname` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
+  `address` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `addr_number` int(11) NOT NULL,
+  `addr_complement` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `addr_district` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `addr_city` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `addr_state` varchar(2) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'PR',
+  `addr_country` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `addr_zip_code` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+  `phone` varchar(14) COLLATE utf8_unicode_ci NOT NULL,
+  `celphone` varchar(15) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- -----------------------------------------------------
--- Table `book_control`.`books`
+-- Table `books`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `book_control`.`books` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
-  `name` VARCHAR(255) NOT NULL ,
-  `resume` TEXT NULL ,
-  `isbn` VARCHAR(45) NULL ,
-  `acquired_form` VARCHAR(45) NULL ,
-  `buyer` VARCHAR(255) NULL ,
-  `purchase_point` TEXT NULL ,
-  `price` DOUBLE(10,2) NULL ,
-  `attachment` TEXT NULL ,
-  `created` DATETIME NULL ,
-  `modified` DATETIME NULL ,
-  PRIMARY KEY (`id`) )
-ENGINE = MyISAM
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_unicode_ci;
+CREATE TABLE `books` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `author` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `resume` text COLLATE utf8_unicode_ci,
+  `isbn` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `acquired_form` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `buyer` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `purchase_point` text COLLATE utf8_unicode_ci,
+  `price` double(10,2) DEFAULT NULL,
+  `attachment` text COLLATE utf8_unicode_ci,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  `publishing_house` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `date_buy` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-
 -- -----------------------------------------------------
--- Table `book_control`.`books_log`
+-- Table `books_logs`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `book_control`.`books_log` (
-  `id` INT NOT NULL ,
-  `book_id` INT UNSIGNED NOT NULL ,
-  `meta_key` TEXT NULL ,
-  `meta_value` TEXT NULL ,
-  PRIMARY KEY (`id`) )
-ENGINE = MyISAM
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_unicode_ci;
+CREATE TABLE `books_logs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `book_id` int(10) unsigned NOT NULL,
+  `meta_key` text COLLATE utf8_unicode_ci,
+  `meta_value` text COLLATE utf8_unicode_ci,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 
 
---
+++ doc/starting.md
@@ -0,0 +1,9 @@
+#Starting
+
+Para iniciar o projeto crie um banco de dados, entre em app/Console e rode o comando:
+
+```shell
+./cake schema create
+```
+
+Instale-o em um server e pronto.
